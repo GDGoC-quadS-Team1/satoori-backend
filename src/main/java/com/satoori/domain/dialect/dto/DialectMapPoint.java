36d945ced394997823d0c96ca310fc0eaa17e814
chaeyylee <ivystar1129@naver.com>
2026-02-03 22:07:31 +0900

    feat: 지도 마커용 방언 좌표 DTO 추가

---
+++ src/main/java/com/satoori/domain/dialect/dto/DialectMapPoint.java
@@ -0,0 +1,17 @@
+package com.satoori.domain.dialect.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DialectMapPoint {
+    private Long dialectId;
+    private String dialect;
+    private String standard;
+    private String region;
+    private Double latitude;
+    private Double longitude;
+}
